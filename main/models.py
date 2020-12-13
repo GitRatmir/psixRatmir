9364a7a4aa45aa985c5f11d6eee048b9082c966e
Ратмир <you@example.com>
2020-12-13 15:41:16 +0300

    test

--- main/models.py
+++ main/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Courses(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название курса')
     content = models.TextField(blank=True, verbose_name='Контент')
-    photo = models.ImageField(upload_to='photos/courses', verbose_name='Фото',blank=True)
     estimation = models.IntegerField(blank=False, verbose_name='Оценка')
     instructor = models.ForeignKey('Instructors', on_delete=models.PROTECT, null=True, verbose_name='Преподователь')
     language = models.CharField(max_length=150,blank=True, verbose_name='Язык')
@@ -19,7 +18,6 @@ class Courses(models.Model):
 class Instructors(models.Model):
     name = models.CharField(max_length=150, verbose_name='Имя Фамилия')
     description = models.TextField(blank=True, verbose_name='Описание')
-    photo = models.ImageField(upload_to='photos/instuctors',verbose_name='Фото',blank=True)
 
     def __str__(self):
         return self.name
